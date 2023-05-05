6eca1e997bad531c9a6a42a39480e2cd1c5bfd5c
Nicolas <nicolas@Nicolass-MacBook-Air.local>
2023-05-05 15:57:45 -0300

    feat: global css setup

---
+++ src/components/constants.ts
@@ -0,0 +1,15 @@
+type ColorVariants =
+  | "primary-blue"
+  | "primary-black"
+  | "primary-white"
+  | "grey-primary"
+  | "grey-medium"
+  | "grey-lighter"
+  | "secondary-green"
+  | "secondary-grey"
+  | "success-green"
+  | "warning-orange"
+  | "transparent"
+  | "error-red";
+
+export default ColorVariants;
